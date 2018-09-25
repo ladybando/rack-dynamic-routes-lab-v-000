29139d60ca7bf49a91efad4800dea0b4c67aa497
Ayana Bando <ayanaflax@yahoo.com>
2018-09-25 14:27:13 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,36 @@
+class Application
+  
+  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
+   
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/item/)
+      item_name = req.path.split("/item/").last
+      if @@items.empty?
+        resp.write "Route not found"
+        resp.status = 404
+      elsif @@items.each do |item|
+        resp.write "#{item}\n"
+        end
+       resp.finish
+     end
+    end
+  end
+end
+
+# def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+ 
+#     if req.path.match(/songs/)
+ 
+#       song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+#       song = @@songs.find{|s| s.title == song_title}
+ 
+#       resp.write song.artist
+#     end
+ 
+#     resp.finish
+#   end
